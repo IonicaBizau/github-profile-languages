94fc22d06b2c1b8158a17f03c08bd30c1d491302
Ionică Bizău <bizauionica@yahoo.com>
2015-03-19 15:49:27 +0200

    Social links

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 $(function () {
+
+    // jQuery Elements
     var $iframe = $("#graph")
       , $input = $(".form-elm")
       , $username = $(".username")
       , $embed = $(".embed textarea")
+      , $twitter = (".popup.twitter")
+      , $facebook = (".popup.facebook")
+      , $gplus = (".popup.gplus")
       , apiUrl = "https://ionicabizau.github.io/github-profile-languages/api.html"
       ;
 
+    /**
+     * check
+     * Updates the user info
+     *
+     * @name check
+     * @function
+     * @return {undefined}
+     */
     function check() {
         var user = unescape(Url.queryString("user"));
         $username.text(user ? "@" + user.replace(/^@/, "") + "'s" : "GitHub Profile");
@@ -13,8 +26,15 @@ $(function () {
         $iframe.attr("src", "api.html?" + user);
         $input.val(user);
         $embed.val("<iframe width='600' height='600' src='" + apiUrl + "?" + user + "' frameborder='0'></iframe>");
+
+        // Update social
+        var escaped = escape(location.href);
+        $facebook.attr("src", "https://www.facebook.com/sharer/sharer.php?u=" + escaped);
+        $twitter.attr("src", "http://twitter.com/intent/tweet?text=A pie graph with my programming languages.&url=" + escaped);
+        $facebook.attr("src", "https://plus.google.com/share?url=" + escaped);
     }
 
+    // Writting the username
     var timer = null;
     $input.on("input", function () {
         Url.updateSearchParam("user", this.value)
@@ -26,12 +46,9 @@ $(function () {
 
     check();
 
-    $(".embed").on("click", function () {
-
-    });
-
-    $(".popup .close").on("click", function () {
-        $(this).closest(".popup").fadeOut();
+    // Popups
+    $("[data-popup]").on("click", function () {
+        $($(this).data("popup")).fadeIn();
     });
 
     $(window).on("keydown", function (e) {
@@ -40,7 +57,7 @@ $(function () {
         }
     });
 
-    $("[data-popup]").on("click", function () {
-        $($(this).data("popup")).fadeIn();
+    $(".popup .close").on("click", function () {
+        $(this).closest(".popup").fadeOut();
     });
 });
