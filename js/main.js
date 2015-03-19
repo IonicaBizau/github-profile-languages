51c4e9afdba8795300d351cad7ed2e9465b2fcc4
Madara Uchiha <me@madara.ninja>
2015-03-19 22:40:37 +0200

    Begin extermination of jQuery.
    Work through most of main.js. Only things that are left are the .fadeOut and .fadeIn calls.
    
    $(fn) => window.onload
    $(selector) => qs/qsa
    jQuery nodes => DOM nodes
    
    Also:
     - escape/unescape => encodeURIComponent/encodeURI/decodeURIComponent
     - Add event delegation

--- js/main.js
+++ js/main.js
@@ -1,64 +1,118 @@
-$(function () {
+(function($, doc) {
+    var qs = doc.querySelector.bind(doc);
+    var qsa = function(selector) {
+        return [].slice.call(doc.querySelectorAll(selector))
+    };
 
-    // jQuery Elements
-    var $iframe = $("#graph")
-      , $input = $(".form-elm")
-      , $username = $(".username")
-      , $embed = $(".embed textarea")
-      , $twitter = $(".popup a.twitter")
-      , $facebook = $(".popup a.facebook")
-      , $gplus = $(".popup a.gplus")
-      , apiUrl = "https://ionicabizau.github.io/github-profile-languages/api.html"
-      ;
 
-    /**
-     * check
-     * Updates the user info
-     *
-     * @name check
-     * @function
-     * @return {undefined}
-     */
-    function check() {
-        var user = unescape(Url.queryString("user"));
-        // TODO XSS
-        $username.html(user ? "<a target='blank' href='https://github.com/" + user + "'>@" + user.replace(/^@/, "") + "</a>'s" : "GitHub Profile");
-        if (!user) { return; }
-        $iframe.attr("src", "api.html?" + user);
-        $input.val(user);
-        $embed.val("<iframe width='600' height='600' src='" + apiUrl + "?" + user + "' frameborder='0'></iframe>");
+    if (!Element.prototype.closest) {
+        /**
+         * Shim for Element.closest
+         *
+         * Goes up in the DOM tree for the context element and
+         * finds elements matching a passed selector.
+         *
+         * @param {string} selector
+         * @returns {Element|null}
+         */
+        Element.prototype.closest = function(selector) {
+            var currentElement = this;
+            while (this.parentNode !== null) {
+                var currentParent = currentElement.parentNode;
+                if (currentParent === null) {
+                    return null;
+                }
+                if (currentParent.querySelector(selector) === currentElement) {
+                    return currentElement;
+                }
 
-        // Update social
-        var escaped = escape(location.href);
-        $facebook.attr("href", "https://www.facebook.com/sharer/sharer.php?u=" + escaped);
-        $twitter.attr("href", "http://twitter.com/intent/tweet?text=A pie graph with my programming languages.&url=" + escaped);
-        $gplus.attr("href", "https://plus.google.com/share?url=" + escaped);
-    }
-
-    // Writting the username
-    var timer = null;
-    $input.on("input", function () {
-        Url.updateSearchParam("user", this.value)
-        clearTimeout(timer);
-        timer = setTimeout(function () {
-            check();
-        }, 1000);
-    });
-
-    check();
-
-    // Popups
-    $("[data-popup]").on("click", function () {
-        $($(this).data("popup")).fadeIn();
-    });
-
-    $(window).on("keydown", function (e) {
-        if (e.which === 27) {
-            $(".popup").fadeOut();
+                currentElement = this.parentNode;
+            }
         }
-    });
+    }
+    window.onload = function() {
 
-    $(".popup .close").on("click", function () {
-        $(this).closest(".popup").fadeOut();
-    });
-});
+        // jQuery Elements
+
+        var iframe = qs('#graph')
+            , input = qs('.form-elm')
+            , username = qs('.username')
+            , embed = qs('.embed textarea')
+            , twitter = qs('.popup a.twitter')
+            , facebook = qs('.popup a.facebook')
+            , gplus = qs('.popup a.gplus')
+            , apiUrl = "https://ionicabizau.github.io/github-profile-languages/api.html"
+            ;
+
+        /**
+         * check
+         * Updates the user info
+         *
+         * @name check
+         * @function
+         * @return {undefined}
+         */
+        function check() {
+            var queryStringUser = Url.queryString("user");
+            var user = decodeURIComponent(queryStringUser);
+            if (!user) {
+                username.textContent = 'GitHub Profile';
+                return
+            }
+            var a = document.createElement('a');
+            a.setAttribute('target', 'blank');
+            a.href = 'https://github.com/' + user;
+            a.textContent = user.replace(/^@/, '');
+
+            username.appendChild(a);
+            username.appendChild(document.createTextNode('\'s'));
+
+            iframe.src = 'api.html?' + user;
+            input.value = user;
+            embed.value = '<iframe width="600" height="600" src="' + apiUrl + '?' + user + '" frameborder="0"></iframe>';
+
+            // Update social
+            var escaped = encodeURI(location.href);
+
+            facebook.href = "https://www.facebook.com/sharer/sharer.php?u=" + escaped;
+            twitter.href = "http://twitter.com/intent/tweet?text=A pie graph with my programming languages.&url=" + escaped;
+            gplus.href = "https://plus.google.com/share?url=" + escaped;
+        }
+
+        // Writing the username
+        var timer = null;
+        input.addEventListener("input", function () {
+            Url.updateSearchParam("user", this.value);
+            clearTimeout(timer);
+            timer = setTimeout(function () {
+                check();
+            }, 1000);
+        });
+
+        check();
+
+        // Popups
+        document.addEventListener('click', function popupStartListener(event) {
+            var target = event.target;
+            if (target.dataset.popup !== undefined) {
+                $(target.dataset.popup).fadeIn();
+            }
+        });
+
+        document.addEventListener('click', function popupCloseListener(event) {
+            var target = event.target;
+            var parentPopup;
+            if (target.classList.contains('close') && (parentPopup = target.closest('.popup')) !== null) {
+                $(parentPopup).fadeOut();
+            }
+        });
+
+
+        window.addEventListener("keydown", function setPopupCloseAllListener(e) {
+            if (e.which === 27) {
+                $(".popup").fadeOut();
+            }
+        });
+
+    };
+})(jQuery, document);
