ab5f13bf4d5e1a88eb2848d060597408d4dfc8b8
Ionică Bizău <bizauionica@yahoo.com>
2015-03-20 12:58:14 +0200

    Code style and other fixes
     - Fixed some minor code style issues.
     - Try to cache data in localStorage and if it fails just clear localStorage.
     - Fixed JSHint errors.

--- js/api.js
+++ js/api.js
@@ -20,12 +20,9 @@
     }
 
     function getJSON(url) {
-        return xhr(url, "get", null)
-            .then(function(data) {
-                console.log(data);
-                return(data);
-            })
-            .then(JSON.parse);
+        return xhr(url, "get", null).then(function(data) {
+            return(data);
+        }).then(JSON.parse);
     }
 
     function serializeFormParameters(parameters) {
@@ -38,80 +35,89 @@
 
     window.addEventListener("load", function apiOnDomReady() {
 
-        var loading = doc.querySelector(".loading");
-        var errorDiv = doc.querySelector(".error");
-        var errorMessage = errorDiv.querySelector(".message");
+        var loading = doc.querySelector(".loading")
+          , errorDiv = doc.querySelector(".error")
+          , errorMessage = errorDiv.querySelector(".message")
+          ;
 
         function getAllRepos(user, page, repos) {
             page = page || 1;
             repos = repos || [];
             if (localStorage[user]) { return Promise.resolve(JSON.parse(localStorage[user])); }
-            return getJSON("https://api.github.com/users/" + user + "/repos?per_page=100&page=" + page)
-                .then(function (newRepos) {
-                    if (newRepos.length) {
-                        repos = repos.concat(newRepos);
-                        return getAllRepos(user, ++page, repos);
-                    }
-                    repos = repos.filter(function (c) {
-                        return !c.fork;
-                    });
-                    if (!repos.length) {
-                        return Promise.reject(new Error("This user doesn't have any repositories."));
-                    }
-                    localStorage[user] = JSON.stringify(repos);
-                    return repos;
-                })
-                .catch(function(err) {
-                    if (err.id < 400 || err.id > 600) { throw err; }
-                    try {
-                        var message = JSON.parse(err.message);
-                    } catch (jsonErr) {
-                        throw err; // The error is not JSON, move on.
-                    }
-                    return Promise.reject(new Error(message.message));
+            return getJSON("https://api.github.com/users/" + user + "/repos?per_page=100&page=" + page).then(function (newRepos) {
+                if (newRepos.length) {
+                    repos = repos.concat(newRepos);
+                    return getAllRepos(user, ++page, repos);
+                }
+
+                repos = repos.filter(function (c) {
+                    return !c.fork;
                 });
+
+                if (!repos.length) {
+                    return Promise.reject(new Error("This user doesn't have any repositories."));
+                }
+
+                try {
+                    localStorage[user] = JSON.stringify(repos);
+                } catch (e) {
+                    localStorage.clear();
+                }
+
+                return repos;
+            }).catch(function(err) {
+                if (err.id < 400 || err.id > 600) { throw err; }
+                var message = null;
+                try {
+                    message = JSON.parse(err.message);
+                } catch (jsonErr) {
+                    throw err; // The error is not JSON, move on.
+                }
+                return Promise.reject(new Error(message.message));
+            });
         }
 
         var user = location.search.replace(/^\?@?/g, "");
         if (!user) {
             return;
         }
+
         loading.classList.add("visible");
 
-        getAllRepos(user)
-            .then(function (repos) {
-                loading.classList.remove("visible");
-                errorDiv.classList.remove("visible");
-                var languages = {};
-                repos.forEach(function (c) {
-                    languages[c.language] = languages[c.language] || 0;
-                    ++languages[c.language];
-                });
-
-                if (languages["null"]) {
-                    languages["Others"] = languages["null"];
-                    delete languages["null"];
-                }
-
-                var arr = Object.keys(languages).map(function (cLang) {
-                    return {
-                        title  : cLang
-                        , value: languages[cLang]
-                        , color: GH_COLORS[cLang]
-                    };
-                });
-                arr.sort(function (a, b) {
-                    return a.value < b.value ? 1 : -1;
-                });
-                drawPieChart.call(doc.querySelector("#pieChart"), arr, {
-                    legend: true
-                });
-            })
-            .catch(function (err) {
-                loading.classList.remove("visible");
-                errorDiv.classList.add("visible");
-                errorMessage.textContent = err || "This user doesn't have any repositories.";
-                throw err; // So that it shows in the console.
+        getAllRepos(user).then(function (repos) {
+            loading.classList.remove("visible");
+            errorDiv.classList.remove("visible");
+            var languages = {};
+            repos.forEach(function (c) {
+                languages[c.language] = languages[c.language] || 0;
+                ++languages[c.language];
             });
+
+            if (languages["null"]) {
+                languages.Others = languages["null"];
+                delete languages["null"];
+            }
+
+            var arr = Object.keys(languages).map(function (cLang) {
+                return {
+                    title: cLang
+                  , value: languages[cLang]
+                  , color: GH_COLORS[cLang]
+                };
+            });
+
+            arr.sort(function (a, b) {
+                return a.value < b.value ? 1 : -1;
+            });
+
+            drawPieChart.call(doc.querySelector("#pieChart"), arr, {
+                legend: true
+            });
+        }).catch(function (err) {
+            loading.classList.remove("visible");
+            errorDiv.classList.add("visible");
+            errorMessage.textContent = err || "This user doesn't have any repositories.";
+            throw err; // So that it shows in the console.
+        });
     });
 })(document);
